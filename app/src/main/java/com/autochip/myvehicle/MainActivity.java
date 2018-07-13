b99376bccac3672f0f4b21350c10e61cd5d7a77c
Vijay <vijayeh.android.com>
2018-07-13 16:16:49 +0530

    Initial Development of MyVehicle app **designing UI **using bottom navigation bar

---
+++ app/src/main/java/com/autochip/myvehicle/MainActivity.java
@@ -0,0 +1,127 @@
+package com.autochip.myvehicle;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    Menu menu;
+
+    private TextView tvTitle, tvSubtitle;
+    private View viewActionBar;
+
+    Toolbar toolbar;
+
+    // FOR NAVIGATION VIEW ITEM TEXT COLOR
+    int[][] states = new int[][]{
+            new int[]{-android.R.attr.state_checked},  // unchecked
+            new int[]{android.R.attr.state_checked},   // checked
+            new int[]{}                                // default
+    };
+
+    // Fill in color corresponding to state defined in state
+    int[] colors = new int[]{
+            Color.parseColor("#757575"),
+            Color.parseColor("#03A9F4"),
+            Color.parseColor("#757575"),
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.navigation_insurance:
+                    tvSubtitle.setText(R.string.title_insurance);
+                    return true;
+                case R.id.navigation_Emission:
+                    tvSubtitle.setText(R.string.title_emission);
+                    return true;
+                case R.id.navigation_rc_fc:
+                    tvSubtitle.setText(R.string.title_rc);
+                    return true;
+                case R.id.navigation_service_history:
+                    tvSubtitle.setText(R.string.title_service_history);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    void init() {
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        viewActionBar = View.inflate(MainActivity.this, R.layout.toolbar_textview, null);
+        tvTitle = viewActionBar.findViewById(R.id.tv_actionbar_app_name);
+        tvTitle.setText(R.string.app_name);
+
+        tvSubtitle = viewActionBar.findViewById(R.id.tv_actionbar_navigation);
+        tvSubtitle.setText(R.string.title_insurance);
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        /*
+        below code sets custom textview to the actionbar title
+         */
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(//set gravity to the textview of the ActionBar !
+                ActionBar.LayoutParams.MATCH_PARENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                Gravity.START);
+        getSupportActionBar().setCustomView(viewActionBar, params);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
+        //mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        ColorStateList navigationViewColorStateList = new ColorStateList(states, colors);
+        navigation.setItemIconTintList(navigationViewColorStateList);
+        navigation.setItemTextColor(navigationViewColorStateList);
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toobar_menu, menu);
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
