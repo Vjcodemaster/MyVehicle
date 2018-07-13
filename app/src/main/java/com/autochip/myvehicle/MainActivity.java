51e51f51596e4155081fc8af8752cb296b3fc267
Vijay <vijayeh.android.com>
2018-07-13 16:28:00 +0530

    Initial Development of MyVehicle app **designing UI **using bottom navigation bar

--- app/src/main/java/com/autochip/myvehicle/MainActivity.java
+++ app/src/main/java/com/autochip/myvehicle/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
